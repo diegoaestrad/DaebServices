a807dec01241e1acfa090a2cd29a89d880e0dde2
Diego Estrada Beltran <86diegoestrada@gmail.com>
2020-12-04 03:51:31 +0000

    basic structure with files , HOme, Services, About us and contact

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'DaebServices - Index' });
+});
+
+/* GET Services page. */
+router.get('/services', function (req, res, next) {
+    res.render('services', { title: 'DaebServices - Services' });
+});
+
+/* GET Services page. */
+router.get('/aboutus', function (req, res, next) {
+    res.render('aboutus', { title: 'DaebServices - About US' });
+});
+
+/* GET Services page. */
+router.get('/contact', function (req, res, next) {
+    res.render('contact', { title: 'DaebServices - Contact Us' });
 });
 
 module.exports = router;
