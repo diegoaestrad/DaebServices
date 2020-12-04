28cac74b2d2f8245d3d390df1a7b9076cf676cc0
Diego Estrada Beltran <86diegoestrada@gmail.com>
2020-12-04 05:32:33 +0000

    website app with tabs information partial update

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'DaebServices - Index' });
+    res.render('index', { title: 'DaebServices - Index', owner: '86diegoestrada', link:"https://www.linkedin.com/in/ingenierodesistemas/" });
 });
 
 /* GET Services page. */
 router.get('/services', function (req, res, next) {
-    res.render('services', { title: 'DaebServices - Services' });
+    res.render('services', { title: 'DaebServices - Services'});
 });
 
 /* GET Services page. */
